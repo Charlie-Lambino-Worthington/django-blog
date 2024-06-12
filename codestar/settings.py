44c7c86d408f4be9da0b959dd5d8723451a72853
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-06-12 14:21:48 +0000

    set debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-charlielambi-djangoblog-vas3elwcs4i.ws-eu114.gitpod.io',
 '.herokuapp.com']
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-charlielambi-djangoblog-vas3elwcs4i.ws-eu114.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
